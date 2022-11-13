ea7ebb1b72ba0c33a9f0b812a312daaafdae288c
Iqbal <iqbal15mirzaee@gmail.com>
2022-11-13 20:07:02 +0430

    edited home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,4 +7,32 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {}
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+                child: Container(
+                    alignment: Alignment.centerLeft,
+                    child: Icon(
+                      Icons.clear_all_rounded,
+                      size: 28,
+                    ))),
+            NotificationBox(
+              number: 1,
+            )
+          ],
+        ),
+      ),
+      body: getBody(),
+    );
+  }
+}
